a229295544e04fcdbdb8ec534695a646e78f4f2b
Anna Di Iulio <anna.diiulio2001@gmail.com>
2024-06-06 18:55:39 +0200

    smt

---
+++ test_1.py
@@ -0,0 +1,143 @@
+from z3 import *
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def read_instance(number):
+    if number < 10:
+        file_path = "instances/inst0" + str(number) + ".dat"  # inserire nome del file
+    else:
+        file_path = "instances/inst" + str(number) + ".dat"  # inserire nome del file
+    variables = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            variables.append(line)
+    #read the number of couriers
+    n_couriers = int(variables[0].rstrip('\n'))
+    #read the number of items
+    n_items = int(variables[1].rstrip('\n'))
+    #read the max load for each courier
+    max_loads = list(map(int, variables[2].rstrip('\n').split()))
+    #read the size of each package
+    sizes = list(map(int, variables[3].rstrip('\n').split()))
+    #read the distances
+    distances_matrix = [list(map(int, line.rstrip('\n').split())) for line in variables[4:]]
+
+    return n_couriers, n_items, max_loads, sizes, distances_matrix
+
+#this is the create graph from the github code, copied and pasted just to try tu understand
+def createGraph(all_distances):
+    num_rows = len(all_distances)
+    num_cols = len(all_distances[0]) if num_rows > 0 else 0
+    all_dist_size = num_rows
+    size_item = num_rows - 1
+    G = nx.DiGraph()
+
+    # Add nodes to the graph
+    G.add_nodes_from(range(all_dist_size))
+
+    # Add double connections between nodes
+    for i in range(all_dist_size):
+        for j in range(i + 1, size_item + 1):  # size item + 1 because we enclude also the depot in the graph
+            G.add_edge(i, j)
+            G.add_edge(j, i)
+
+    # Assign edge lengths
+    lengths = {(i, j): all_distances[i][j] for i, j in G.edges()}
+    nx.set_edge_attributes(G, lengths, 'length')
+
+    return G
+
+def exactly_one(vars):
+    return And(
+        Or(vars),  # At least one is true
+        Not(Or([And(vars[i], vars[j]) for i in range(len(vars)) for j in range(i+1, len(vars))]))
+    )
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+n_couriers = 2
+n_items = 7
+max_loads = [15, 10]
+sizes = [3, 2, 6, 5, 4, 4]
+distances = [
+    [0, 3, 4, 5, 6, 6, 2],
+    [3, 0, 1, 4, 5, 7, 3],
+    [4, 1, 0, 5, 6, 6, 4], 
+    [4, 4, 5, 0, 3, 3, 2],
+    [6, 7, 8, 3, 0, 2, 4],
+    [6, 7, 8, 3, 2, 0, 4], 
+    [2, 3, 4, 3, 4, 4, 0]
+]
+
+max_time = 300
+s = Solver()
+
+#define a timeout for the whole thing, as specified in the project assignment
+s.set("timeout", (int(max_time) * 1000))
+
+#create the graph containing all possible paths
+G = createGraph(distances)
+
+#create the decision variables
+
+#courier load for courier k
+cour_load = [Int(f'Courier_load_for_{k}') for k in range(n_couriers)] 
+
+#x[i][j][k] = True means that the route from i to j is used by courier k
+x = [[[Bool(f"x_{i}_{j}_{k}") for k in range(n_couriers)] for j in range(n_items + 1)] for i in
+         range(n_items + 1)]
+
+#Constraints -------------------------------------------------------------------------------------------------------------------------------
+
+#the load must be smaller than the max_loads and greater than 0
+for k in range(n_couriers):
+    const_1 = cour_load[k]>0
+    const_2 = cour_load[k]<max_loads[k]
+    s.add(const_1)
+    s.add(const_2)
+
+#each courier must start and end at origin point o
+o = 0
+for k in range(n_couriers):
+    #I used sum because the amount of x[o][j][k] (or the other one) that needs to be True (aka 1) is exactly one
+    #therefore if I sum all of those, the sum needs to be 1, as they are all 0 and only one is 1
+    const_3 = Sum([x[o][j][k] for j in G.nodes if j != 0]) == 1
+    const_4 = Sum([x[i][o][k] for i in G.nodes if i != 0]) == 1
+    s.add(const_3)
+    s.add(const_4)
+
+#each node is visited at most once by the same courier
+for k in range(n_couriers):
+    #I consider both the "normal" points and the origin
+    for j in G.nodes:
+        const_5 = Sum([x[i][j][k] for i in G.nodes if i != j]) <= 1
+        s.add(const_5)
+
+#each node must be entered and left once by the same courier
+#meaning that the amount of times a courier enters the node is the exact same amount as it leaves the node
+for k in range(n_couriers):
+    #I consider both the "normal" points and the origin
+    for j in G.nodes:
+        a = Sum([x[i][j][k] for i in G.nodes if i != j]) 
+        b = Sum([x[j][i][k] for i in G.nodes if i != j]) 
+        const_6 = (a==b)
+        s.add(const_6)
+
+#there is no route from a node to itself
+for k in range(n_couriers):
+    const_7 = Sum([x[i][i][k] for i in G.nodes]) == 0
+    s.add(const_7)
+
+#every item needs to be delivered by whichever courier
+for j in G.nodes:
+        if j != o:  # the origin point o is not considered
+            #this means that every 3d column of the output must have only one truth value = True
+            const_8 = exactly_one([x[i][j][k] for k in range(n_couriers) for i in G.nodes if i != j])
+            s.add(const_8)
+
+#every item must be delivered only once
+#constraint of the upper bounds of items a courier can bring
+
+
